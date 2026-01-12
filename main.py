ed992bd996d4f18c00922601693e91c58e54cb5b
Yem Daro <daroyem99@gmail.com>
2026-01-12 22:18:47 +0700

    add hsv filter

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import cv2
+import matplotlib.pyplot as plt
 import src.filter as filter
 
 def main():
-    print("Hello from shmold!")
 
-    filter.sharpen_and_eq_hist()
+    data_dir = 'data/project-17-at-2026-01-12-12-18-01b29e6f'
+    output_dir = 'data/output/hsv5'
+
+    filter.change_hsv_and_save(data_dir, output_dir, -15, 5, 2)
+
+    # img_path = 'data/project-17-at-2026-01-12-12-18-01b29e6f/images/008daf3a-Dropped_Image_124.png'
+
+    # img = cv2.imread(img_path)
+    # img = filter.hsv(img, -20, -5, 0)
+
+    # cv2.imshow('change hsv', img)
+
+    # while True:
+    #     key = cv2.waitKey(1) & 0xFF
+    #     if key == 27:  # ESC key
+    #         break
+
+    # cv2.destroyAllWindows()
+    # filter.sharpen_and_eq_hist()
 
 if __name__ == "__main__":
     main()
--- src/filter.py
+++ src/filter.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+from cv2.typing import MatLike
 import numpy as np
 import random
 import shutil
@@ -57,4 +58,57 @@ def sharpen_and_eq_hist():
         if os.path.exists(src_label_path):
             shutil.copy(src_label_path, dst_label_path)
 
-print("Augmentation complete. Images and labels saved in:", augmented_path)        
+def hsv(img: MatLike, hue=0, sat=0, val=0):
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV).astype(np.int16)
+    h, s, v = cv2.split(hsv)
+
+    h = (h + hue) % 180        # wrap hue
+    s = np.clip(s + sat, 0, 255)
+    v = np.clip(v + val, 0, 255)
+
+    hsv_edited = cv2.merge([h, s, v]).astype(np.uint8)
+    img_edited = cv2.cvtColor(hsv_edited, cv2.COLOR_HSV2BGR)
+    return img_edited
+
+
+def change_hsv_and_save(data_dir: str, output_dir: str, hue=0, sat=0, val=0):
+    # Create folders
+    os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(output_dir, exist_ok=True)
+
+    image_dir = os.path.join(data_dir, 'images')
+    label_dir = os.path.join(data_dir, 'labels')
+
+    out_img_dir = os.path.join(output_dir, 'images')
+    out_label_dir = os.path.join(output_dir, 'labels')
+
+    os.makedirs(out_img_dir, exist_ok=True)
+    os.makedirs(out_label_dir, exist_ok=True)
+
+
+    suffix = 'hsv'
+    for filename in os.listdir(image_dir):
+        if not filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp')):
+            continue
+
+        img_path = os.path.join(image_dir, filename)
+
+        img = cv2.imread(img_path)
+        img = hsv(img, hue, sat, val)
+        
+        # Get file name and extension separately
+        name, ext = os.path.splitext(filename)
+
+        # Save augmented images
+        save_name = f'{name}_{suffix}{ext}'
+        save_path = os.path.join(out_img_dir, save_name)
+        _ = cv2.imwrite(save_path, img)
+
+
+        # Copy all labels from OG file
+        label_file = name + '.txt'
+        src_label_path = os.path.join(label_dir, label_file)
+        dst_label_path = os.path.join(out_label_dir, f'{name}_{suffix}.txt')
+        if os.path.exists(src_label_path):
+            _ = shutil.copy(src_label_path, dst_label_path)
+
