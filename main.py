66f9cae1e595cf355ef862fdce9c7d3e65af6a1f
Yem Daro <daroyem99@gmail.com>
2026-01-12 22:33:10 +0700

    add rgb filter

--- main.py
+++ main.py
@@ -5,23 +5,35 @@ import src.filter as filter
 def main():
 
     data_dir = 'data/project-17-at-2026-01-12-12-18-01b29e6f'
-    output_dir = 'data/output/hsv5'
 
-    filter.change_hsv_and_save(data_dir, output_dir, -15, 5, 2)
 
-    # img_path = 'data/project-17-at-2026-01-12-12-18-01b29e6f/images/008daf3a-Dropped_Image_124.png'
+    rgb_output_dir = 'data/output/rgb5'
+    hsv_output_dir = 'data/output/hsv5'
 
-    # img = cv2.imread(img_path)
-    # img = filter.hsv(img, -20, -5, 0)
+    # filter.change_hsv_and_save(data_dir, hsv_output_dir, -15, 5, 2)
+    # filter.change_bgr_and_save(data_dir, rgb_output_dir, -50, 25, -20)
 
-    # cv2.imshow('change hsv', img)
+    img_path = 'data/project-17-at-2026-01-12-12-18-01b29e6f/images/008daf3a-Dropped_Image_124.png'
 
-    # while True:
-    #     key = cv2.waitKey(1) & 0xFF
-    #     if key == 27:  # ESC key
-    #         break
+    img = cv2.imread(img_path)
+    # img = filter.rgb(img, 2, 20, 40)
+    img1 = filter.add_rgb(img, -10, 20, 16)
 
-    # cv2.destroyAllWindows()
+    img2 = filter.add_hsv(img, -12, 4, 0)
+
+
+    cv2.imshow('change rgb', img1)
+    cv2.imshow('change hsv', img2)
+
+
+    while True:
+        key = cv2.waitKey(1) & 0xFF
+        if key == 27:  # ESC key
+            break
+
+    cv2.destroyAllWindows()
+
+
     # filter.sharpen_and_eq_hist()
 
 if __name__ == "__main__":
--- src/filter.py
+++ src/filter.py
@@ -58,7 +58,7 @@ def sharpen_and_eq_hist():
         if os.path.exists(src_label_path):
             shutil.copy(src_label_path, dst_label_path)
 
-def hsv(img: MatLike, hue=0, sat=0, val=0):
+def add_hsv(img: MatLike, hue=0, sat=0, val=0):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV).astype(np.int16)
     h, s, v = cv2.split(hsv)
 
@@ -94,8 +94,61 @@ def change_hsv_and_save(data_dir: str, output_dir: str, hue=0, sat=0, val=0):
         img_path = os.path.join(image_dir, filename)
 
         img = cv2.imread(img_path)
-        img = hsv(img, hue, sat, val)
+        img = add_hsv(img, hue, sat, val)
         
+        # Get file name and extension separately
+        name, ext = os.path.splitext(filename)
+
+        # Save augmented images
+        save_name = f'{name}_{suffix}{ext}'
+        save_path = os.path.join(out_img_dir, save_name)
+        _ = cv2.imwrite(save_path, img)
+
+
+        # Copy all labels from OG file
+        label_file = name + '.txt'
+        src_label_path = os.path.join(label_dir, label_file)
+        dst_label_path = os.path.join(out_label_dir, f'{name}_{suffix}.txt')
+        if os.path.exists(src_label_path):
+            _ = shutil.copy(src_label_path, dst_label_path)
+
+def add_rgb(img: MatLike, red=0, green=0, blue=0):
+    img_int16 = img.astype(np.int16)
+    b, g, r = cv2.split(img_int16)
+
+    b = np.clip(b + blue, 0, 255)
+    g = np.clip(g + green, 0, 255)
+    r = np.clip(r + red, 0, 255)
+
+    bgr_edited = cv2.merge([b, g, r]).astype(np.uint8)
+    return bgr_edited
+
+
+def change_bgr_and_save(data_dir: str, output_dir: str, red=0, green=0, blue=0):
+    # Create folders
+    os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(output_dir, exist_ok=True)
+
+    image_dir = os.path.join(data_dir, 'images')
+    label_dir = os.path.join(data_dir, 'labels')
+
+    out_img_dir = os.path.join(output_dir, 'images')
+    out_label_dir = os.path.join(output_dir, 'labels')
+
+    os.makedirs(out_img_dir, exist_ok=True)
+    os.makedirs(out_label_dir, exist_ok=True)
+
+
+    suffix = 'rgb'
+    for filename in os.listdir(image_dir):
+        if not filename.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp')):
+            continue
+
+        img_path = os.path.join(image_dir, filename)
+
+        img = cv2.imread(img_path)
+        img = add_rgb(img, red, green, blue)
+
         # Get file name and extension separately
         name, ext = os.path.splitext(filename)
 
